4fbc2a4d820ade7a0cdb0452f606aacadea58f3e
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2023-12-14 10:41:16 +0530

    added comment

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
-const express = require("express")
-const app = express()
+// Importing the 'express' module to create a web server.
+const express = require("express");
 
+// Creating an Express application.
+const app = express();
+
+// Defining a route for handling GET requests to the "/ping" endpoint.
 app.get("/ping", (req, res) => {
-    res.send("Hello World")
-})
+    // Sending the response "Hello World" for GET requests to "/ping".
+    res.send("Hello World");
+});
 
-app.listen(3000, () => { console.log("Server Started") })
+// Starting the web server on port 3000 and logging a message when the server starts.
+app.listen(3000, () => {
+    console.log("Server Started");
+});
