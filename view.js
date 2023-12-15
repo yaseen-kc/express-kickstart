e1ec759c17a0dede6abd48b5f7e5d56bf8298381
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2023-12-15 14:49:08 +0530

    dd

--- view.js
+++ view.js
@@ -1,10 +1,18 @@
+// Importing the 'express' module for creating a web server.
 const express = require("express");
+
+// Creating an Express application.
 const app = express();
 
+// Setting the port number for the server.
 const PORT = 3000;
 
-app.set('view engine', 'pug')
+// Configuring the view engine to use 'pug'.
+app.set('view engine', 'pug');
 
+// Handling GET requests to the root ('/') endpoint and rendering the 'index' view.
 app.get('/', function (req, res) {
-    res.render('index')
-}).listen(PORT, () => { console.log("http://localhost:3000") });
+    res.render('index', { title: "Express View Engine" });
+}).listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+});
