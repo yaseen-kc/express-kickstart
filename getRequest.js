f6efab30e27780c79a8a73ea6820c4d4e0c34282
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2023-12-30 20:56:36 +0530

    get method build: update package.json

---
+++ getRequest.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const fs = require("fs");
+
+let app = express();
+let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
+
+
+app.get('/api/v1/movies', (req, res) => {
+    res.status(200).json(movies)
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log("Server Has Started");
+})
